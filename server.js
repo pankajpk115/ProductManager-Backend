b7367319b413adc0b9828f1978e27c90eea2394d
pankajpk115 <pankajpkpk909@gmail.com>
2025-10-26 11:39:17 +0530

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { corsMiddleware } from "./config/cors.js";
-import productRoutes from "./routes/productRoutes.js";
+import productRoutes from "./routes/productRoute.js";
 
 dotenv.config();
 const app = express();
