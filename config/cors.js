86001448df3e5f9bc4b60dbef3c20eb109685ff9
pankajpk115 <pankajpkpk909@gmail.com>
2025-10-26 11:20:38 +0530

    deploy

--- config/cors.js
+++ config/cors.js
@@ -3,7 +3,6 @@ import cors from "cors";
 const allowedOrigins = [
   "http://localhost:5173",
   "https://product-manager-frontend-psi.vercel.app",
-  "https://productmanager-backend-vd2u.onrender.com"
 ];
 
 const corsOptions = {
@@ -17,5 +16,4 @@ const corsOptions = {
   credentials: true,
 };
 
-
-export default cors(corsOptions);
+export const corsMiddleware = cors(corsOptions);
--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import productRoutes from "./routes/productRoute.js";
-import corsMiddleware from "./config/cors.js"
-
-dotenv.config();
+import { corsMiddleware } from "./config/cors.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
+app.use(express.json());
 app.use(corsMiddleware);
+
 app.use("/api/products", productRoutes);
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.error("❌ MongoDB Error:", err));
+app.get("/", (req, res) => {
+  res.send("✅ Product Manager API is running");
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
