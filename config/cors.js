8eaabecb7bc4654ede95c561f80577ce9de9d9aa
pankajpk115 <pankajpkpk909@gmail.com>
2025-10-26 11:01:34 +0530

    deploy

--- config/cors.js
+++ config/cors.js
@@ -1,19 +1,21 @@
-import cors from 'cors';
+import cors from "cors";
 
 const allowedOrigins = [
-  'http://localhost:5173',
-  'https://product-manager-frontend-psi.vercel.app',
-  'https://productmanager-backend-vd2u.onrender.com'
+  "http://localhost:5173",
+  "https://product-manager-frontend-psi.vercel.app",
+  "https://productmanager-backend-vd2u.onrender.com"
 ];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.some(url => origin.startsWith(url))) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error(`CORS blocked for origin: ${origin}`));
+      callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
 };
+
+
+export default cors(corsOptions);
